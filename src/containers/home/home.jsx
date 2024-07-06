b2eb97b0ec624034e928caaa48892d239669db27
Margo <margarita.cortizas@gmail.com>
2024-07-06 02:07:06 -0300

    Home

--- src/containers/home/home.jsx
+++ src/containers/home/home.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import './home.css'
 import { useNavigate } from 'react-router-dom'
+import image from '../../assets/logo.jpg'
 
 const Home =() => {
     const navigate = useNavigate()
-    
+   
     return(
-    <div>
-        <button onClick = {() => navigate("/difficulties")}> Jugar </button>
-    </div>
+        <>
+           <div className="home">
+                <div>
+                    <div>
+                        <button  onClick = {() => navigate("/difficulties")}> Play! </button>
+                    </div>  
+                </div>
+            </div>
+        </>
     )
 }
 
