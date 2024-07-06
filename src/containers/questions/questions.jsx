ab37b4ce8bd1eee593415511dcf3a6fde8e61e5f
Margo <margarita.cortizas@gmail.com>
2024-07-06 00:19:14 -0300

    Preguntas según dificultad

---
+++ src/containers/questions/questions.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './questions.css'
+import { useNavigate } from 'react-router-dom'
+
+const Questions =() => {
+    const navigate = useNavigate()
+    
+    return(
+    <div>
+        <div>
+            {/* pregunta a la appi */}
+        </div>
+        <button>  </button>
+        <button>  </button>
+        <button>  </button>
+        <button>  </button> 
+    </div>
+    )
+}
+
+export default Questions;
